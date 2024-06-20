0264d2250f72b3a526d5fc7109935e5d0fe8bd91
CoderDude2 <isaacboots1@gmail.com>
2024-06-20 10:09:52 -0700

    WIP gui.py

--- gui.py
+++ gui.py
@@ -1,9 +1,27 @@
-import tkinter as tk
+import tkinter as tk 
+from tkinter import ttk
+from dataclasses import dataclass
+
+@dataclass
+class Associate:
+    name:str
+    case_types:list[str]
 
 class App:
     def __init__(self) -> None:
         self.root:tk.Tk = tk.Tk()
-        self.root.geometry("300x300")
+        self.root.geometry("400x300")
+
+        file_viewer = ttk.Treeview(columns=('case_id', 'two'), show='headings')
+        file_viewer.heading("case_id", text="ID")
+        file_viewer.heading("two", text="Two")
+
+        file_viewer.insert(parent='',index=tk.END, values=(1111, "Test"))
+
+        file_viewer.pack()
+        
+    def new_associate(self) -> None:
+        pass
     
     def run(self) -> None:
         self.root.mainloop()
