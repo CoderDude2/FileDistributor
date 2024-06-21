6bafbbe0ff26247e3e9650d07d02e14906106c93
CoderDude2 <isaacboots1@gmail.com>
2024-06-20 18:00:40 -0700

    WIP gui.py

--- gui.py
+++ gui.py
@@ -10,15 +10,15 @@ class Associate:
 class App:
     def __init__(self) -> None:
         self.root:tk.Tk = tk.Tk()
-        self.root.geometry("400x300")
+        self.root.geometry("600x300")
 
-        file_viewer = ttk.Treeview(columns=('case_id', 'two'), show='headings')
+        file_viewer = ttk.Treeview(columns=('case_id', 'name'), show='headings')
         file_viewer.heading("case_id", text="ID")
-        file_viewer.heading("two", text="Two")
+        file_viewer.heading("name", text="Name")
 
-        file_viewer.insert(parent='',index=tk.END, values=(1111, "Test"))
+        file_viewer.insert(parent='',index=tk.END, values=(6311, "PDO-PL-0406311__(NAR-CS-TA14,6311).stl"))
 
-        file_viewer.pack()
+        file_viewer.pack(fill="x")
         
     def new_associate(self) -> None:
         pass
